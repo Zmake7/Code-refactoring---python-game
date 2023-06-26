7ccff17d9c350285634822390c896d767749b1bb
Zmake7 <zhoutiancheng84@gmail.com>
2023-06-26 22:51:09 +0700

    #8#Длинный метод#__check_bullets_crates(self)函数实际上包含两个功能模块，1，子弹和箱子碰撞效果；2，不同的箱子具有不同的功能。我们把第二个提取成一个方法apply_crate_effect

--- logic.py
+++ logic.py
@@ -19,6 +19,7 @@ from gfx import *
 from units import *
 import pygame
 
+# 处理游戏得分
 class Points(object):
     def __init__(self):
         """
@@ -38,38 +39,11 @@ class Points(object):
     def get_points(self):
         return self.__points
 
+# 初始化游戏逻辑
 class Destroyer_logic(object):
 
     def __init__(self, timer, destroyer, destroyer_options, enemies, bullets, torpedos, explosions, fades, texts,
                  points, crates, window_size):
-
-        """
-        This is where all the game logic magic happens. Takes the instances of the different game objects and checks
-        for e.g. collisions. Defines what happens when collisions are detected.
-
-        :param destroyer    : game instance of destroyer class
-        :param enemies      : game instance of enemies class
-        :param bullets      : game instance of bullets class
-        :param torpedos     : game instance of torpedos class
-        :param explosions   : game instance of Explosions class
-        :param fades        : game instance of Fades class
-        :param texts        : game instance of Texts class
-        :param points       : game instance of points class
-        :param crates       : game instance of crates class
-        :param window_size  : window size as x(int), y(int)
-        :type destroyer     : Destroyer
-        :type enemies       : Enemies
-        :type bullets       : Bullets
-        :type torpedos      : Torpedos
-        :type explosions    : Explosions
-        :type fades         : Fades
-        :type texts         : Texts
-        :type points        : Points
-        :type crates        : Crates
-        :type window_size   : list
-
-        :returns:
-        """
 
         self.__destroyer = destroyer
         self.__bullets = bullets
@@ -84,14 +58,8 @@ class Destroyer_logic(object):
         self.__timer = timer
         self.__destroyer_options = destroyer_options
 
+    # 检查是否有窗口外的子弹。返回要删除的子弹索引列表
     def __check_bullets(self):
-
-        """
-        Checks if any of the bullets are outside the game window. Returns a list of indices for the bullets that are
-        to be removed.
-
-        :returns: list
-        """
         bullet_remove_list = []
         bullet_list = self.__bullets.get_bullets()
 
@@ -101,14 +69,8 @@ class Destroyer_logic(object):
                 bullet_remove_list.append(b)
         return bullet_remove_list
 
+    # 检查子弹和敌人之间的碰撞以及这种情况下会发生什么。返回要删除的子弹和敌人的列表。
     def __check_bullets_enemies(self):
-
-        """
-        Checks for collisions between bullets and enemies and what happens in that case. Returns list of bullets and
-        enemies that are to be removed.
-
-        :returns: list
-        """
         enemy_remove_list = []
         bullet_remove_list = []
         enemy_list = self.__enemies.get_enemies()
@@ -135,57 +97,46 @@ class Destroyer_logic(object):
                     self.__destroyer.reduce_hp(_bullet.get_damage())
         return bullet_remove_list, enemy_remove_list
 
+    # 检查敌人是否超出游戏窗口。返回一个需要移除的敌人列表。
     def __check_enemies(self):
-
-        """
-        Checks if any of the enemies are outside the game window. Returns a list of enemies that are to be removed
-
-        :returns: list
-        """
         enemies_remove_list = []
         for e, _enemy in enumerate(self.__enemies.get_enemies()):
             rect = _enemy.get_extent()
-
+            # 如果敌人向上移动且超出窗口，则将其从列表中移除并减少玩家的分数。
             if _enemy.get_direction() == 0:
                 if rect[1] <= 0:
                     enemies_remove_list.append(e)
                     self.__points.reduce_points(_enemy.get_params()["points"])
-
+            # 如果敌人向右移动且超出窗口，则将其从列表中移除并减少玩家的分数。
             if _enemy.get_direction() == 1:
                 if rect[0] >= self.__window_size[0]:
                     enemies_remove_list.append(e)
                     self.__points.reduce_points(_enemy.get_params()["points"])
-
+            # 如果敌人向下移动且超出窗口，则将其从列表中移除并减少玩家的分数。
             if _enemy.get_direction() == 2:
                 if rect[1] > self.__window_size:
                     enemies_remove_list.append(e)
                     self.__points.reduce_points(_enemy.get_params()["points"])
-
+            # 如果敌人向左移动且超出窗口，则将其从列表中移除并减少玩家的分数。
             if _enemy.get_direction() == 3:
                 if rect[2] <= 0:
                     enemies_remove_list.append(e)
                     self.__points.reduce_points(_enemy.get_params()["points"])
         return enemies_remove_list
 
+    # 检查鱼雷是否超出游戏窗口。返回一个需要移除的鱼雷列表。
     def __check_torpedos(self):
-
-        """
-        Checks if any of the torpedos are outside the game window. Returns a list of torpedos that are to be removed
-
-        :returns: list
-        """
-
         torpedos_remove_list = []
         for t, _torpedo in enumerate(self.__torpedos.get_torpedos()):
             rect = _torpedo.get_rect()
-
+            # 如果鱼雷击中了驱逐舰，则将其从列表中移除，并添加爆炸效果、减少驱逐舰的生命值和玩家的分数。
             if _torpedo.get_image()[1].colliderect(self.__destroyer.get_image()[1]):
                 torpedos_remove_list.append(t)
                 self.__explosions.add_explosion(Explosion(_torpedo.get_position(), 20))
                 self.__texts.add_text(_torpedo.get_position(), "-{}".
                                       format(_torpedo.get_params()["points"]), positive = False)
                 self.__destroyer.reduce_hp(_torpedo.get_damage())
-
+            # 如果鱼雷向上，右，下，左移动且超出窗口，则将其从列表中移除。
             elif _torpedo.get_direction() == 0:
                 if rect[1] <= 0:
                     torpedos_remove_list.append(t)
@@ -200,26 +151,23 @@ class Destroyer_logic(object):
                     torpedos_remove_list.append(t)
         return torpedos_remove_list
 
+    # 检查子弹和鱼雷之间的碰撞，以及在这种情况下会发生什么。返回要删除的子弹和鱼雷的列表。
     def __check_bullets_torpedos(self):
-
-        """
-        Checks for collisions between bullets and torpedos and what happens in that case. Returns list of bullets
-        torpedos that are to be removed.
-
-        :returns: list
-        """
-
+        # 初始化要移除的鱼雷和子弹的列表
         torpedo_remove_list = []
         bullet_remove_list = []
         torpedo_list = self.__torpedos.get_torpedos()
         bullet_list = self.__bullets.get_bullets()
-
+        # 遍历子弹列表
         for b,_bullet in enumerate(bullet_list):
             if _bullet.is_friendly():
+                # 遍历鱼雷列表
                 for t, _torpedo in enumerate(torpedo_list):
+                    # 如果子弹和鱼雷发生碰撞，将它们添加到要移除的列表中
                     if _bullet.get_image()[1].colliderect(_torpedo.get_image()[1]):
                         bullet_remove_list.append(b)
                         torpedo_remove_list.append(t)
+                        # 添加得分、爆炸、淡出和文本效果
                         self.__points.add_points(_torpedo.get_params()["points"])
                         self.__explosions.add_explosion(Explosion(_bullet.get_position(), 20))
                         self.__fades.add_fade(_torpedo.get_image()[0], _torpedo.get_image()[1], 0.5)
@@ -227,84 +175,62 @@ class Destroyer_logic(object):
                                               format(_torpedo.get_params()["points"]))
         return bullet_remove_list, torpedo_remove_list
 
+    # Defining the effect of each type of crate
+    # 定义每种类型箱子的效果
+    def apply_crate_effect(self, crate, bullet_position):
+        if crate.get_type() == 0:
+            self.__destroyer.increase_hp(crate.get_effect_points())
+            self.__texts.add_text(bullet_position, "+{}hp".format(crate.get_effect_points()))
+        elif crate.get_type() == 1:
+            self.__destroyer.increase_max_hp(crate.get_effect_points())
+            self.__texts.add_text(bullet_position, "+{} max hp".format(crate.get_effect_points()))
+        elif crate.get_type() == 2:
+            self.__destroyer.reset_hp()
+            self.__texts.add_text(bullet_position, "HP refilled!")
+        elif crate.get_type() == 3:
+            for e in self.__enemies.get_enemies():
+                self.__bullets.add_bullet(Destroyer_bullet_1(self.__timer, e.get_center_point(), 0))
+            self.__texts.add_text(bullet_position, "C'EST LA BOMBE!")
+        elif crate.get_type() == 4:
+            x = crate.get_position()[0]
+            y = crate.get_position()[1]
+            self.__bullets.add_bullet(Mine(self.__timer, (x, y - 40), 0))
+            self.__bullets.add_bullet(Mine(self.__timer, (x + 40, y), 0))
+            self.__bullets.add_bullet(Mine(self.__timer, (x, y + 40), 0))
+            self.__bullets.add_bullet(Mine(self.__timer, (x - 40, y), 0))
+            self.__texts.add_text(bullet_position, "Mines!")
+        elif crate.get_type() == 5:
+            self.__destroyer_options.set_reload_time(100, 10)
+            self.__destroyer_options.set_power_reduction(0, 10)
+            self.__destroyer_options.set_power_refill(500, 10)
+            self.__texts.add_text(bullet_position, "M..m...machine gun!!!")
+            self.__destroyer_options.set_text_timer(10)
+
+    # 检查子弹和箱子之间的碰撞，以及在这种情况下会发生什么。返回要删除的子弹和箱子的列表。
     def __check_bullets_crates(self):
-
-        """
-        Checks for collisions between bullets and crates and what happens in that case. Returns list of bullets and
-        crates that are to be removed.
-
-        :returns: list
-        """
-
+        # 初始化要移除的箱子和子弹的列表
         bullet_remove_list = []
         crate_remove_list = []
         bullet_list = self.__bullets.get_bullets()
         crate_list = self.__crates.get_crates()
 
+        # 遍历子弹列表
         for b, _bullet in enumerate(bullet_list):
             if _bullet.is_friendly():
+                # 遍历箱子列表
                 for c, _crate in enumerate(crate_list):
+                    # 如果子弹和箱子发生碰撞，将它们添加到要移除的列表中
                     if _bullet.get_image()[1].colliderect(_crate.get_rect()):
                         bullet_remove_list.append(b)
                         crate_remove_list.append(c)
+                        # 添加得分和爆炸效果
                         self.__points.add_points(_crate.get_points())
                         self.__explosions.add_explosion(Explosion(_bullet.get_position(), 20))
-
-                        #Defining the effect of each type of crate
-                        if _crate.get_type() == 0:
-                            self.__destroyer.increase_hp(_crate.get_effect_points())
-                            self.__texts.add_text(_bullet.get_position(), "+{}hp".
-                                                  format(_crate.get_effect_points()))
-
-                        if _crate.get_type() == 1:
-                            self.__destroyer.increase_max_hp(_crate.get_effect_points())
-                            self.__texts.add_text(_bullet.get_position(), "+{} max hp".
-                                              format(_crate.get_effect_points()))
-
-                        if _crate.get_type() == 2:
-                            self.__destroyer.reset_hp()
-                            self.__texts.add_text(_bullet.get_position(), "HP refilled!".
-                                                  format(_crate.get_effect_points()))
-
-                        if _crate.get_type() == 3:
-                            for e in self.__enemies.get_enemies():
-                                self.__bullets.add_bullet(Destroyer_bullet_1(self.__timer,
-                                                                             e.get_center_point(), 0))
-                            self.__texts.add_text(_bullet.get_position(), "C'EST LA BOMBE!".
-                                                  format(_crate.get_effect_points()))
-
-                        if _crate.get_type() == 4:
-                            x = _crate.get_position()[0]
-                            y = _crate.get_position()[1]
-
-                            self.__bullets.add_bullet(Mine(self.__timer,(x, y-40), 0))
-                            self.__bullets.add_bullet(Mine(self.__timer,(x+40, y), 0))
-                            self.__bullets.add_bullet(Mine(self.__timer,(x, y+40), 0))
-                            self.__bullets.add_bullet(Mine(self.__timer,(x-40, y), 0))
-
-                            self.__texts.add_text(_bullet.get_position(), "Mines!".
-                                                  format(_crate.get_effect_points()))
-                            break
-
-                        if _crate.get_type() == 5:
-                            self.__destroyer_options.set_reload_time(100,10)
-                            self.__destroyer_options.set_power_reduction(0,10)
-                            self.__destroyer_options.set_power_refill(500,10)
-                            self.__texts.add_text(_bullet.get_position(), "M..m...machine gun!!!".
-                                                  format(_crate.get_effect_points()))
-                            self.__destroyer_options.set_text_timer(10)
-
+                        self.apply_crate_effect(_crate, _bullet.get_position())
 
         return bullet_remove_list, crate_remove_list
 
     def __check_enemies_crates(self):
-
-        """
-        Checks for collisions between enemies and crates and what happens in that case. Returns list of crates that
-        are to be removed.
-
-        :returns: list
-        """
         crates_remove_list = []
         crate_list = self.__crates.get_crates()
         for e, _enemy in enumerate(self.__enemies.get_enemies()):
@@ -314,17 +240,6 @@ class Destroyer_logic(object):
         return crates_remove_list
 
     def check(self):
-
-        """
-        Runs the collision check functions above. The returned lists containing the list indices of the objects that
-        are to be deleted are combined per object type. The lists are then "destilled" down to unique indices in
-        order to prevent double entries leading to problems during removal. Calls the remove methods for each object
-        type with the "destilled" list as argument. Spawns a new enemy in case all enemies are gone. Returns true if
-        the destroyer has been destroyed.
-
-        :returns: boolean
-        """
-
         bullet_remove_list_1 = self.__check_bullets()
         bullet_remove_list_2, enemy_remove_list_1 = self.__check_bullets_enemies()
         enemy_remove_list_2 = self.__check_enemies()
@@ -347,6 +262,7 @@ class Destroyer_logic(object):
         if len(self.__enemies.get_enemies()) == 0:
             self.__enemies.add_enemy()
 
+        # 将本轮中击败的敌人数量添加到总数中
         #Add the number of enemies sunk in this round to the total count
         self.__enemies.inc_sunk_count(len(enemy_remove_list_1))
 
